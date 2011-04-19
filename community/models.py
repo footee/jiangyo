4be9c259d9ce1a9a48f69faccf60dd0697a405b1
footee <footee.peng@gmail.com>
2011-04-19 20:46:37 +0800

    Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   anjuke.db
    	new file:   anjuke_utf8.lua
    	new file:   anjuke_utf8.lua~
    	modified:   community/models.py
    	new file:   community/models.py~
    	new file:   community/urls.py~
    	modified:   community/views.py
    	new file:   community/views.py~
    	new file:   settings.py~
    	new file:   subway_data.txt
    	new file:   subway_utf8.lua
    	new file:   subway_utf8.lua~
    	new file:   templates/index.html~
    	new file:   test.csv
    	new file:   urls.py~

--- community/models.py
+++ community/models.py
@@ -12,48 +12,48 @@ from datetime import datetime
 
 class Area(models.Model):
     # id = models.IntegerField(null=True, primary_key=True, blank=True)
-    url = models.TextField(null=False, blank=False)
-    name = models.TextField(null=False, blank=False)
-    community = models.IntegerField(null=False, blank=False)
+    url = models.TextField(null=False, blank=False)             # 区域anjuke的url http://beijing.anjuke.com/v2/community/list/<url>
+    name = models.TextField(null=False, blank=False)            # 区域名 (朝阳, 海淀等)
+    community = models.IntegerField(null=False, blank=False)    # 区域内小区个数
     class Meta:
         db_table = u'area'
 
 class CommunityInfo(models.Model):
     # id = models.IntegerField(null=True, primary_key=True, blank=True)
-    url = models.IntegerField(null=False, blank=False)
-    name = models.TextField(null=False, blank=False)
+    url = models.IntegerField(null=False, blank=False)          # 小区anjuke的url, 在anjuke中也是一个唯一的ID http://beijing.anjuke.com/v2/community/view/<url>
+    name = models.TextField(null=False, blank=False)            # 小区name
     latitude = models.DecimalField(null=True, blank=True, max_digits=18, decimal_places=15)
     longitude = models.DecimalField(null=True, blank=True, max_digits=18, decimal_places=15)
-    location = models.TextField(null=True, blank=True)
-    sale_trends = models.TextField(null=True, blank=True)
-    rental_trends = models.TextField(null=True, blank=True)
-    houses = models.IntegerField(null=True, blank=True)
-    pack = models.TextField(null=True, blank=True)
-    developer = models.TextField(null=True, blank=True)
-    tenement = models.TextField(null=True, blank=True)
-    tenement_type = models.TextField(null=True, blank=True)
-    building_date = models.DateField(null=True, blank=True)
-    plot_ratio = models.DecimalField(null=True, blank=True, max_digits=6, decimal_places=3)
-    afforest = models.DecimalField(null=True, blank=True, max_digits=6, decimal_places=3)
-    area = models.TextField(null=False, blank=False)
-    timestamp = models.DateTimeField(auto_now=True, auto_now_add=True)
+    location = models.TextField(null=True, blank=True)          # 中文地址
+    sale_trends = models.TextField(null=True, blank=True)       # 价格趋势, anjuke的url http://beijing.anjuke.com/v2/community/trends/<sale_trends>
+    rental_trends = models.TextField(null=True, blank=True)     # 租金趋势, anjuke的url http://beijing.haozu.com/compound/trends/<rental_trends>
+    houses = models.IntegerField(null=True, blank=True)         # 小区总户数
+    pack = models.TextField(null=True, blank=True)              # 小区停车位个数
+    developer = models.TextField(null=True, blank=True)         # 开发商
+    tenement = models.TextField(null=True, blank=True)          # 物业公司
+    tenement_type = models.TextField(null=True, blank=True)     # 物业类型 (公寓, 别墅等)
+    building_date = models.DateField(null=True, blank=True)     # 竣工日期
+    plot_ratio = models.DecimalField(null=True, blank=True, max_digits=6, decimal_places=3)     # 容积率
+    afforest = models.DecimalField(null=True, blank=True, max_digits=6, decimal_places=3)       # 绿化率
+    area = models.TextField(null=False, blank=False)                                            # 所属区域
+    timestamp = models.DateTimeField(auto_now=True, auto_now_add=True)                          # 数据更新时间戳
     class Meta:
         db_table = u'community_info'
 
 class CommunityPrice(models.Model):
     # id = models.IntegerField(null=True, primary_key=True, blank=True)
     info = models.ForeignKey(CommunityInfo)
-    price = models.IntegerField(null=True, blank=True)
-    sales = models.IntegerField(null=True, blank=True)
-    sale_1 = models.IntegerField(null=True, blank=True)
-    sale_2 = models.IntegerField(null=True, blank=True)
-    sale_3 = models.IntegerField(null=True, blank=True)
-    sale_4 = models.IntegerField(null=True, blank=True)
-    sale_5 = models.IntegerField(null=True, blank=True)
-    sale_6 = models.IntegerField(null=True, blank=True)
-    rental = models.IntegerField(null=True, blank=True)
-    rents = models.IntegerField(null=True, blank=True)
-    timestamp = models.DateTimeField(auto_now=True, auto_now_add=True)
+    price = models.IntegerField(null=True, blank=True)          # 当前平均售价 元/平米
+    sales = models.IntegerField(null=True, blank=True)          # 当前出售房源
+    sale_1 = models.IntegerField(null=True, blank=True)         # 一室出售房源
+    sale_2 = models.IntegerField(null=True, blank=True)         # 二室出售房源
+    sale_3 = models.IntegerField(null=True, blank=True)         # 三室出售房源
+    sale_4 = models.IntegerField(null=True, blank=True)         # 四室出售房源
+    sale_5 = models.IntegerField(null=True, blank=True)         # 五室出售房源
+    sale_6 = models.IntegerField(null=True, blank=True)         # 五室以上出售房源
+    rental = models.IntegerField(null=True, blank=True)         # 当前平均租金 元/套
+    rents = models.IntegerField(null=True, blank=True)          # 当前出租房源
+    timestamp = models.DateTimeField(auto_now=True, auto_now_add=True)      # 数据创建时间戳
     class Meta:
         db_table = u'community_price'
 
--- community/views.py
+++ community/views.py
@@ -11,6 +11,7 @@ from django.template import RequestContext, Context, Template
 from django.utils.http import urlquote
 from django.conf import settings
 import json
+import math
 
 
 
@@ -29,4 +30,17 @@ def community(request):
     print(data)
 
     return HttpResponse(data)
+
+
+def distance(lat1, lng1, lat2, lng2):
+    earth_radius = 6378137 # unit: meter
+    radLat1 = math.radians(lat1)
+    radLat2 = math.radians(lat2)
+    a = radLat1 - radLat2
+    b = rad(lng1) - rad(lng2)
+    s = 2 * math.asin(math.sqrt(math.pow(math.sin(a / 2), 2) +
+        math.cos(radLat1) * math.cos(radLat2) * math.pow(math.sin(b / 2), 2)))
+    s = s * earth_radius
+    s = math.round(s * 100) / 100
+    return s
 
