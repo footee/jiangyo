ea2ccbed93639f93e1b10d3731e2042f3ca415bf
footee <footee.peng@gmail.com>
2011-04-24 23:44:14 +0800

    modified:   anjuke.db 	modified:   geocode.lua
     Untracked files:
       (use "git add <file>..." to include in what will be committed)
    
    	geocode.lua~

--- geocode.lua
+++ geocode.lua
@@ -10,6 +10,9 @@ local uregex = require("icu.regex")
 local sqlite3 = require("luasql.sqlite3")
 local json = require("json")
 
+local env = assert(luasql.sqlite3())
+local con = assert(env:connect("anjuke.db"))
+
 
 function httpwget(url)
     -- get the community link and name
@@ -30,13 +33,44 @@ function httpwget(url)
     return page, upage
 end
 
-local page, upage = httpwget("http://ditu.google.cn/maps/geo?output=json&sensor=false&q=瑞丽家园")
 
-data = json.decode(page)
+function rows(connection, sql_statement)
+	local cursor = assert(connection:execute(sql_statement))
+	return function ()
+		return cursor:fetch()
+	end
+end
 
-lng = data['Placemark'][1]['Point']['coordinates'][1]
-lat = data['Placemark'][1]['Point']['coordinates'][2]
+local page
+local upage
+action = "SELECT * from community_info where latitude is NULL"
+for id,url,name,lat,lng,address,sale_trends,rental_trends,houses,pack,developer,tenement,tenement_type,building_date,plot_ratio,afforest,area,timestamp  in rows(con, action) do 
+	
+	print(id, name, address, area)
+		
+	repeat 
+		page, upage = httpwget("http://ditu.google.cn/maps/geo?output=json&sensor=false&q=".."北京市"..address) 
+	until page ~= nil
 
-print(lat, lng)
+	data = json.decode(page)
+	
+	if data['Placemark'] == nil then
+		page, upage = httpwget("http://ditu.google.cn/maps/geo?output=json&sensor=false&q=".."北京市"..address..name)
+	end
+	data = json.decode(page)
+
+	lng = data['Placemark'][1]['Point']['coordinates'][1]
+	lat = data['Placemark'][1]['Point']['coordinates'][2]
+
+	if lat ~= 39.904214 then
+		print(lat, lng)
+		action = string.format([[UPDATE community_info SET latitude=%.15f , longitude=%.15f where id=%s]], lat, lng, id)
+	else
+		print("NULL, NULL")
+		action = string.format([[UPDATE community_info SET latitude=NULL , longitude=NULL where id=%s]], id)
+	end
+	
+	res = con:execute(action)
+end
 
 
